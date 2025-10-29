5aaef93b457b90c8eaa0141c49996f1942fc95c2
Zhiyang Jin <z76jin@uwaterloo.ca>
2025-10-29 17:46:06 -0400

    setup and practice simpliest code

---
+++ 01-hello/hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, James!\n");
+    return 0;
+}
---
+++ 04-initialization/initialization.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int var=3;
+    var =4;
+    int var1=5, var2;
+    var2=var1;
+    printf("var:%d\n", var);
+    printf("var1:%d\n", var1);
+    printf("var2:%d", var2);
+    return 0;
+}
